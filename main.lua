7a93415671fb95759e93de93168e13f8b6b8d8e8
3aze <skidjssjj+CUPaiAtOXYMU4g@googlemail.com>
2025-05-06 18:00:21 -0700

    Update main.lua
    im smart

--- main.lua
+++ main.lua
@@ -6,9 +6,9 @@ if _G.__CHESS_AI_LOADED__FULL then
 end
 _G.__CHESS_AI_LOADED__FULL = true
 
-modules.config = loadstring(game:HttpGet("https://raw.githubusercontent.com/SchizoCore/chessBot/main/config.lua"))()
-modules.state = loadstring(game:HttpGet("https://raw.githubusercontent.com/SchizoCore/chessBot/main/core/state.lua"))()
-modules.ai = loadstring(game:HttpGet("https://raw.githubusercontent.com/SchizoCore/chessBot/main/core/ai.lua"))()
-modules.gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/SchizoCore/chessBot/main/core/gui.lua"))()
+modules.config = loadstring(game:HttpGet("https://raw.githubusercontent.com/3aze/bot/main/config.lua"))()
+modules.state = loadstring(game:HttpGet("https://raw.githubusercontent.com/3aze/bot/main/core/state.lua"))()
+modules.ai = loadstring(game:HttpGet("https://raw.githubusercontent.com/3aze/bot/main/core/ai.lua"))()
+modules.gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/3aze/bot/main/core/gui.lua"))()
 
 modules.gui.init(modules)
